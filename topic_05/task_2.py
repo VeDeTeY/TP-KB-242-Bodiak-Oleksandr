99e4d4f0c61dffcd1213222e50354d3a6306154a
Bodiak Oleksandr <alex064413@gmail.com>
2025-11-12 10:25:43 +0200

    chenged task_02

--- topic_05/task_2.py
+++ topic_05/task_2.py
@@ -1,28 +1,36 @@
 import requests
 
-############ Отримання курсу валюти з API НБУ #############
-def get_rate(currency):
-    url = f"https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?valcode={currency}&json"
-    response = requests.get(url)
-    file = response.json()
-    return file[0]['rate']
+############ Отримання всіх курсів валют з API НБУ #############
+response = requests.get("https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json")
 
-############ Основа конвертатора #############
+def rates():
+    for elem in response.json():
+        if (elem["cc"] == "USD" or elem["cc"] == "EUR" or elem["cc"] == "PLN"):
+            print(f"{elem['cc']} : {elem['rate']}")
+
+############ Введення валюти #############
+def input_currency():
+    currency = input("Введіть код валюти (USD, EUR, PLN): ").upper()
+    return currency
+
+############ Введення суми #############
+def input_amount():
+    amount = float(input("Введіть суму валюти: "))
+    return amount
+
+############ Основна функція #############
 def main():
     print("Конвертер валют (USD, EUR, PLN → UAH)")
-    currency = input("Введіть валюту (USD, EUR або PLN): ").upper()
-    amount = float(input("Введіть суму: "))
-
-    if currency not in ["USD", "EUR", "PLN"]:
-        print("Невірна валюта! Спробуйте ще раз.")
-        return
-
-    rate = get_rate(currency)
-    result = amount * rate
-
-    print(f"1 {currency} = {rate:.2f} грн")
-    print(f"{amount:.2f} {currency} = {result:.2f} грн")
+    
+    # Отримуємо дані від користувача
+    currency = input_currency()
+    amount = input_amount()
+    
+    # Конвертуємо
+    for elem in response.json():
+        if elem["cc"] == currency:
+            converted_amount = amount * elem["rate"]
+            print(f"\n{amount} {currency} = {converted_amount:.2f} UAH")
 
 ############### Запуск ###############
-if __name__ == "__main__":
-    main()
+main()
