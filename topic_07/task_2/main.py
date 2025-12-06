aac75e2a5b60bb0f2f98f6a8f18fc82d43913a99
Bodiak Oleksandr <alex064413@gmail.com>
2025-12-06 11:49:47 +0200

    added topic_07

---
+++ topic_07/task_2/main.py
@@ -0,0 +1,15 @@
+from calc import Calculator
+
+calc = Calculator()
+
+############# Операція #############
+operation = input("Виберіть операцію (+, -, *, /): ")
+
+############## Ввід чисел ##############
+a = float(input("Введіть перше число: "))
+b = float(input("Введіть друге число: "))
+
+########### Обчислення ############
+result = calc.calculate(a, b, operation)
+
+print("Результат:", result)
